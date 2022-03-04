1f485b75e36cccf7d6635e51120fbca0538077f0
Taylor Barnes <taylor.a.barnes@gmail.com>
2022-03-04 12:44:44 -0500

    Update plugin_driver.py

--- tests/driver/plugin_driver.py
+++ tests/driver/plugin_driver.py
@@ -1,55 +1,85 @@
 import mdi
 from mdi import MDI_NAME_LENGTH, MDI_COMMAND_LENGTH
 import sys
-import mpi4py
-#mpi4py.rc.initialize = False
 from mpi4py import MPI
 
-def code_for_plugin_instance(mpi_comm, mdi_comm, class_object):
 
-    mdi.MDI_Send_Command("<NAME", mdi_comm)
-    name = mdi.MDI_Recv(mdi.MDI_NAME_LENGTH, mdi.MDI_CHAR, mdi_comm)
-    print("Engine name: " + str(name))
+# This function is called by the MDI Library, not by this Driver
+# It wraps the PluginInstance.callback function for the MDI Library,
+#    which cannot directly call class functions.
+def callback_wrapper(mpi_comm, mdi_comm, class_object):
+    return class_object.callback(mpi_comm, mdi_comm)
 
-    cell = [
-        12.0, 0.0, 0.0,
-        0.0, 12.0, 0.0,
-        0.0, 0.0, 12.0 ]
 
-    elements = [ 8, 1, 1 ]    
+class PluginInstance:
 
-    coords = [
-        0.0, -0.553586, 0.0,
-        1.429937, 0.553586, 0.0,
-        -1.429937, 0.553586, 0.0
-        ]
 
-    natoms = len( elements )
+    def __init__(self):
 
-    mdi.MDI_Send_Command(">CELL", mdi_comm)
-    mdi.MDI_Send(cell, 9, mdi.MDI_DOUBLE, mdi_comm)
+        # Set system information for this plugin run
+        # For some engines, this information will be sent via MDI
+        # For others, this information must be read in the engine's input file
+        self.cell = [
+            12.0, 0.0, 0.0,
+            0.0, 12.0, 0.0,
+            0.0, 0.0, 12.0 ]
+        self.elements = [ 8, 1, 1 ]    
+        self.coords = [
+            0.0, -0.553586, 0.0,
+            1.429937, 0.553586, 0.0,
+            -1.429937, 0.553586, 0.0
+            ]
+        self.natoms = len( self.elements )
 
-    mdi.MDI_Send_Command("<CELL", mdi_comm)
-    cell = mdi.MDI_Recv(9, mdi.MDI_DOUBLE, mdi_comm)
-    print("Cell: " + str(cell))
 
-    mdi.MDI_Send_Command(">NATOMS", mdi_comm)
-    mdi.MDI_Send(natoms, 1, mdi.MDI_INT, mdi_comm)
+    # launch the plugin
+    def launch(self, plugin_name, command_line, mpi_world):
 
-    mdi.MDI_Send_Command(">ELEMENTS", mdi_comm)
-    mdi.MDI_Send(elements, natoms, mdi.MDI_INT, mdi_comm)
+        mdi.MDI_Launch_plugin(plugin_name,
+                      str(command_line) + " -mdi \"-name MM -role ENGINE -method LINK\"",
+                      mpi_world,
+                      callback_wrapper,
+                      self)
 
-    mdi.MDI_Send_Command(">COORDS", mdi_comm)
-    mdi.MDI_Send(coords, 3*natoms, mdi.MDI_DOUBLE, mdi_comm)
 
-    mdi.MDI_Send_Command("<ENERGY", mdi_comm)
-    energy = mdi.MDI_Recv(1, mdi.MDI_DOUBLE, mdi_comm)
-    print("ENERGY: " + str(energy))
+    # This function is called by the MDI Library, not by this Driver
+    # It contains the commands that should be sent to the engine
+    def callback(self, mpi_comm, mdi_comm):
 
-    # Send the "EXIT" command to the engine
-    mdi.MDI_Send_Command("EXIT", mdi_comm)
+        mdi.MDI_Send_Command("<NAME", mdi_comm)
+        name = mdi.MDI_Recv(mdi.MDI_NAME_LENGTH, mdi.MDI_CHAR, mdi_comm)
+        print("Engine name: " + str(name))
 
-    return 0
+        mdi.MDI_Send_Command(">CELL", mdi_comm)
+        mdi.MDI_Send(self.cell, 9, mdi.MDI_DOUBLE, mdi_comm)
+
+        mdi.MDI_Send_Command("<CELL", mdi_comm)
+        self.cell = mdi.MDI_Recv(9, mdi.MDI_DOUBLE, mdi_comm)
+        print("Cell: " + str(self.cell))
+
+        mdi.MDI_Send_Command(">NATOMS", mdi_comm)
+        mdi.MDI_Send(self.natoms, 1, mdi.MDI_INT, mdi_comm)
+
+        mdi.MDI_Send_Command(">ELEMENTS", mdi_comm)
+        mdi.MDI_Send(self.elements, self.natoms, mdi.MDI_INT, mdi_comm)
+
+        mdi.MDI_Send_Command(">COORDS", mdi_comm)
+        mdi.MDI_Send(self.coords, 3*self.natoms, mdi.MDI_DOUBLE, mdi_comm)
+
+        mdi.MDI_Send_Command("<ENERGY", mdi_comm)
+        energy = mdi.MDI_Recv(1, mdi.MDI_DOUBLE, mdi_comm)
+        print("ENERGY: " + str(energy))
+        
+        mdi.MDI_Send_Command("<FORCES", mdi_comm)
+        forces = mdi.MDI_Recv(3*self.natoms, mdi.MDI_DOUBLE, mdi_comm)
+        print("FORCES: " + str(forces))
+
+        # Send the "EXIT" command to the engine
+        mdi.MDI_Send_Command("EXIT", mdi_comm)
+
+        return 0
+
+
 
 iarg = 1
 while iarg < len(sys.argv):
@@ -66,75 +96,15 @@ while iarg < len(sys.argv):
 
     iarg += 1
 
-# Connect to the engine
-#comm = mdi.MDI_Accept_Communicator()
+
 plugin_name = "inqmdi"
-#plugin_name = "engine_cxx"
 mpi_world = MPI.COMM_WORLD
-mdi.MDI_Launch_plugin(plugin_name,
-                      "-mdi \"-name MM -role ENGINE -method LINK\"",
-                      mpi_world,
-                      code_for_plugin_instance,
-                      None)
+engine_command_line = ""
+
+
+for i in range(2):
+    plugin = PluginInstance()
+    plugin.launch(plugin_name, engine_command_line, mpi_world)
 
 
 
-print("------------- AFTER FIRST CALL ---------------------")
-
-
-mdi.MDI_Launch_plugin(plugin_name,
-                      "-mdi \"-name MM -role ENGINE -method LINK\"",
-                      mpi_world,
-                      code_for_plugin_instance,
-                      None)
-
-
-
-
-
-
-
-# Get the name of the engine, which will be checked and verified at the end
-#mdi.MDI_Send_Command("<NAME", comm)
-#initial_name = mdi.MDI_Recv(mdi.MDI_NAME_LENGTH, mdi.MDI_CHAR, comm)
-
-##############
-print("AAA")
-
-
-# Verify that the engine is still responsive
-#mdi.MDI_Send_Command("<NAME", comm)
-#final_name = mdi.MDI_Recv(mdi.MDI_NAME_LENGTH, mdi.MDI_CHAR, comm)
-#print("Engine name: " + str(final_name))
-
-#mdi.MDI_Send_Command("<CELL", comm)
-#cell = mdi.MDI_Recv(9, mdi.MDI_DOUBLE, comm)
-#print("Cell: " + str(cell))
-
-#mdi.MDI_Send_Command("<CELL_DISPL", comm)
-#cell_displ = mdi.MDI_Recv(3, mdi.MDI_DOUBLE, comm)
-#print("Cell_displ: " + str(cell_displ))
-
-#mdi.MDI_Send_Command("<PE", comm)
-#pe = mdi.MDI_Recv(1, mdi.MDI_DOUBLE, comm)
-#print("PE: " + str(pe))
-
-#mdi.MDI_Send_Command("<KE", comm)
-#ke = mdi.MDI_Recv(1, mdi.MDI_DOUBLE, comm)
-#print("KE: " + str(ke))
-
-#mdi.MDI_Send_Command("<ENERGY", comm)
-#energy = mdi.MDI_Recv(1, mdi.MDI_DOUBLE, comm)
-#print("ENERGY: " + str(energy))
-
-#mdi.MDI_Send_Command("<NATOMS", comm)
-#natoms = mdi.MDI_Recv(1, mdi.MDI_INT, comm)
-#print("NATOMS: " + str(natoms))
-
-#mdi.MDI_Send_Command("<FORCES", comm)
-#forces = mdi.MDI_Recv(3*natoms, mdi.MDI_DOUBLE, comm)
-#print("FORCES: " + str(forces))
-
-#print("CCC")
-
-#mdi.MDI_Send_Command("EXIT", comm)
